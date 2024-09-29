89797e3af6a7e36916c7c3345b025b7845e0aa21
arelhan <are255gu@gmail.com>
2024-09-30 00:42:33 +0300

    Connectionstring yazıldı

--- obj/Debug/net8.0/StoreApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6239ca96327f8b49e345bfd7ca8b54c3e485abd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93a5325040c15546c38e5e8e66ff51fa4324a46e")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
