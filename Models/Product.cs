93a5325040c15546c38e5e8e66ff51fa4324a46e
arelhan <are255gu@gmail.com>
2024-09-30 00:29:19 +0300

    Paketler ve Repositoryler tanimlandi

---
+++ Models/Product.cs
@@ -0,0 +1,11 @@
+namespace StoreApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }= string.Empty;
+        public decimal Price { get; set; } = 0;
+
+
+    }
+}
